e08e01057817e77fe26325412be4a75c9f444c18
Orrin Manning <orrin.manning@protonmail.com>
2022-07-18 23:42:09 -0500

    create template for printing site-logo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let path = '';
+
+  let user = {
+    name: ''
+  };
+</script>
+
+<div class="logo-container">
+  <h1>
+    <code style=" display: flex; flex-wrap: false">
+      <p class="logo-string">{'['}</p>
+      {#if user?.name}
+        <p class="logo-string">{`${user?.name}@`}</p>
+      {/if}
+      <p class="logo-string">orrin.io</p>
+      {#if path}
+        <p class="logo-string logo-path">{` ${path}`}</p>
+      {/if}
+      <p class="logo-string">{']$'}</p>
+    </code>
+  </h1>
+</div>
+
+<style>
+  .logo-container {
+    display: flex;
+    justify-content: center;
+  }
+
+  .logo-string {
+    margin: 0;
+  }
+
+  .logo-path {
+    white-space: pre;
+  }
+</style>
