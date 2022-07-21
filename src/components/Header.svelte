ec774be852f9eac4869614e0e0672b8a158e0616
Orrin Manning <orrin.manning@protonmail.com>
2022-07-21 01:17:41 -0500

    Extract Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let username: string | null = null;
+  export let path: string | null = null;
+</script>
+
+<div class="site-header">
+  <h1>
+    <code style=" display: flex; flex-wrap: false">
+      <p class="logo-string">{'['}</p>
+      {#if username}
+        <p class="logo-string">{`${username}@`}</p>
+      {/if}
+      <p class="logo-string">orrin.io</p>
+      {#if path}
+        <p class="logo-string logo-path">{` ${path}`}</p>
+      {/if}
+      <p class="logo-string">{']$'}</p>
+    </code>
+  </h1>
+</div>
+
+<style>
+  .site-header {
+    display: flex;
+    justify-content: center;
+  }
+
+  .logo-string {
+    margin: 0;
+  }
+
+  .logo-path {
+    white-space: pre;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
-<script>
+<script lang="ts">
+  import Header from '../components/Header.svelte';
   let path = '';
 
   let user = {
@@ -6,33 +7,4 @@
   };
 </script>
 
-<div class="logo-container">
-  <h1>
-    <code style=" display: flex; flex-wrap: false">
-      <p class="logo-string">{'['}</p>
-      {#if user?.name}
-        <p class="logo-string">{`${user?.name}@`}</p>
-      {/if}
-      <p class="logo-string">orrin.io</p>
-      {#if path}
-        <p class="logo-string logo-path">{` ${path}`}</p>
-      {/if}
-      <p class="logo-string">{']$'}</p>
-    </code>
-  </h1>
-</div>
-
-<style>
-  .logo-container {
-    display: flex;
-    justify-content: center;
-  }
-
-  .logo-string {
-    margin: 0;
-  }
-
-  .logo-path {
-    white-space: pre;
-  }
-</style>
+<Header />
