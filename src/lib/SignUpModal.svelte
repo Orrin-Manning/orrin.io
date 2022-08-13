add51f7bb6746ab093c4c196e325f7a86eeb45f1
Orrin Manning <orrin.manning@protonmail.com>
2022-08-12 23:38:29 -0500

    Add header

--- src/lib/SignUpModal.svelte
+++ src/lib/SignUpModal.svelte
@@ -5,11 +5,12 @@
 
 {#if visible}
   <div class="flex flex-col px-2 pt-16 pb-8 fixed inset-0 bg-black bg-opacity-75">
-    <div class="flex flex-col p-8 h-full max-w-sm bg-white rounded-3xl">
+    <div class="flex flex-col items-center p-8 max-w-sm bg-white rounded-3xl">
       <button on:click={hideSignup} class="self-end w-8 h-8 relative">
         <div class="line rotate-45" />
         <div class="line -rotate-45" />
       </button>
+      <h1 class="mb-8 font-bold text-3xl">Sign Up</h1>
     </div>
   </div>
 {/if}
